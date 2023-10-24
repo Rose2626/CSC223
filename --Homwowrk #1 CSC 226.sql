900b06b40475803e9601e2d3038cf963532355ab
Rose2626 <144184665+Rose2626@users.noreply.github.com>
2023-10-24 18:16:52 -0400

    Add files via upload
    HomeWork#1

---
+++ --Homwowrk #1 CSC 226.sql
@@ -0,0 +1,118 @@
+--Homwowrk #1 CSC 226
+
+
+
+CREATE TABLE RoseExtenstions (
+    HairSpecifics varchar(255) PRIMARY KEY,
+    HairType varchar(255),
+    HairLength varchar(255),
+    HairDensity varchar(255),
+    HairColor varchar(255),
+    HairExtension varchar(255) 
+);
+
+
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('1' ,'Loose Wave', '8 inches' , '120%', 'Black' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('2' ,'Kinky', '8 inches' , '120%', 'Black' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('3' ,'Body Wave', '8 inches' , '120%', 'Black' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('4' ,'Romance Curl', '8 inches' , '120%', 'Black' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('5' ,'Deep Wave', '8 inches' , '120%', 'Black' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('6' ,'Jerry Curl', '8 inches' , '120%', 'Black' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('7' ,'Spanish Curl', '8 inches' , '120%', 'Black' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('8' ,'Wet n Wavy', '8 inches' , '120%', 'Black' , 'Bundles');
+
+
+
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('9' ,'Loose Wave', '14 inches' , '160%', 'Brown' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('10' ,'Kinky', '14 inches' , '160%', 'Brown' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('11' ,'Body Wave', '14 inches' , '160%', 'Brown' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('12' ,'Romance Curl', '14 inches' , '160%', 'Brown' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('13' ,'Deep Wave', '14 inches' , '160%', 'Brown' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('14' ,'Jerry Curl', '14 inches' , '160%', 'Brown' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('15' ,'Spanish Curl', '14 inches' , '160%', 'Brown' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('16' ,'Wet n Wavy', '14 inches' , '160%', 'Brown' , 'Bundles');
+
+
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('17' ,'Loose Wave', '22 inches' , '180%', 'Burgundy' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('18' ,'Kinky', '22 inches' , '180%', 'Burgundy' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('19' ,'Body Wave', '22 inches' , '180%', 'Burgundy' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('20' ,'Romance Curl', '22 inches' , '180%', 'Burgundy' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('21' ,'Deep Wave', '22 inches' , '180%', 'Burgundy' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('22' ,'Jerry Curl', '22 inches' , '180%', 'Burgundy' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('23' ,'Spanish Curl', '22 inches' , '180%', 'Burgundy' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('24' ,'Wet n Wavy', '22 inches' , '180%', 'Burgundy' , 'Bundles');
+
+
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('25' ,'Loose Wave', '28 inches' , '200%', 'Ginger' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('26' ,'Kinky', '28 inches' , '200%', 'Ginger' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('27' ,'Body Wave', '28 inches' , '200%', 'Ginger' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('28' ,'Romance Curl', '28 inches' , '200%', 'Ginger' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('29' ,'Deep Wave', '28 inches' , '200%', 'Ginger' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('30' ,'Jerry Curl', '28 inches' , '200%', 'Ginger' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('31' ,'Spanish Curl', '28 inches' , '200%', 'Ginger' , 'Bundles');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('32' ,'Wet n Wavy', '28 inches' , '200%', 'Ginger' , 'Bundles');
+
+DELETE FROM RoseExtenstions WHERE HairLength ='22 inches';
+
+UPDATE RoseExtenstions
+SET HairExtension = 'Clipins', HairColor = 'Blonde'
+WHERE HairSpecifics = '9';
+
+
+UPDATE RoseExtenstions
+SET HairExtension = 'Crochet Braids', HairColor = 'Jet Black'
+WHERE HairSpecifics = '12';
+
+
+UPDATE RoseExtenstions
+SET HairExtension = 'Faux locc', HairColor = 'Honey Blonde'
+WHERE HairSpecifics = '13';
+
+UPDATE RoseExtenstions
+SET HairExtension = 'Frontal', HairColor = 'Dark Brown'
+WHERE HairSpecifics = '15';
+
+UPDATE RoseExtenstions
+SET HairExtension = 'Clouser', HairColor = 'Red'
+WHERE HairSpecifics = '16';
+
+
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('33' ,'Loose Wave', '8 inches' , '120%', 'Black' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('32' ,'Kinky', '8 inches' , '120%', 'Black' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('35' ,'Body Wave', '8 inches' , '120%', 'Black' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('36' ,'Romance Curl', '8 inches' , '120%', 'Black' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('37' ,'Deep Wave', '8 inches' , '120%', 'Black' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('38' ,'Jerry Curl', '8 inches' , '120%', 'Black' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('39' ,'Spanish Curl', '8 inches' , '120%', 'Black' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('40' ,'Wet n Wavy', '8 inches' , '120%', 'Black' , 'Wigs');
+
+
+
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('41' ,'Loose Wave', '14 inches' , '160%', 'Brown' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('42' ,'Kinky', '14 inches' , '160%', 'Brown' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('43' ,'Body Wave', '14 inches' , '160%', 'Brown' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('44' ,'Romance Curl', '14 inches' , '160%', 'Brown' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('45' ,'Deep Wave', '14 inches' , '160%', 'Brown' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('46' ,'Jerry Curl', '14 inches' , '160%', 'Brown' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('47' ,'Spanish Curl', '14 inches' , '160%', 'Brown' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('48' ,'Wet n Wavy', '14 inches' , '160%', 'Brown' , 'Wigs');
+
+
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('49' ,'Loose Wave', '22 inches' , '180%', 'Burgundy' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('50' ,'Kinky', '22 inches' , '180%', 'Burgundy' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('51' ,'Body Wave', '22 inches' , '180%', 'Burgundy' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('52' ,'Romance Curl', '22 inches' , '180%', 'Burgundy' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('53' ,'Deep Wave', '22 inches' , '180%', 'Burgundy' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('54' ,'Jerry Curl', '22 inches' , '180%', 'Burgundy' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('55' ,'Spanish Curl', '22 inches' , '180%', 'Burgundy' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('56' ,'Wet n Wavy', '22 inches' , '180%', 'Burgundy' , 'Wigs');
+
+
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('57' ,'Loose Wave', '28 inches' , '200%', 'Ginger' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('58' ,'Kinky', '28 inches' , '200%', 'Ginger' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('59' ,'Body Wave', '28 inches' , '200%', 'Ginger' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('60' ,'Romance Curl', '28 inches' , '200%', 'Ginger' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('61' ,'Deep Wave', '28 inches' , '200%', 'Ginger' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('62' ,'Jerry Curl', '28 inches' , '200%', 'Ginger' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('63' ,'Spanish Curl', '28 inches' , '200%', 'Ginger' , 'Wigs');
+INSERT INTO RoseExtenstions (HairSpecifics,HairType,HairLength,HairDensity,HairColor,HairExtension) VALUES ('64' ,'Wet n Wavy', '28 inches' , '200%', 'Ginger' , 'Wigs');
